2ce6e16e4128285bb0dfd7e7305a7bbbb712c11d
dchaconz98 <diegochz98@gmail.com>
2021-09-15 11:04:36 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# OACAPlanificacinPorcinos
+# OACAPlanificacionPorcinos
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'OACAPlanificacinPorcinos'`, () => {
+  it(`should have as title 'OACAPlanificacionPorcinos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('OACAPlanificacinPorcinos');
+    expect(app.title).toEqual('OACAPlanificacionPorcinos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('OACAPlanificacinPorcinos app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('OACAPlanificacionPorcinos app is running!');
   });
 });
