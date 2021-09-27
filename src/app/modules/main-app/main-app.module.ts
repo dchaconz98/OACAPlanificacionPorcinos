7fcf3317b3c7639df3a93ed43169ece62486be8b
Diego <dchacon@unpluggedcorp.com>
2021-09-27 12:40:37 -0400

    Agrega avances de pagina de simulacion

--- src/app/modules/main-app/main-app.module.ts
+++ src/app/modules/main-app/main-app.module.ts
@@ -7,6 +7,12 @@ import { SimulationComponent } from './pages/simulation/simulation.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 
 @NgModule({
@@ -20,6 +26,11 @@ import {MatCardModule} from '@angular/material/card';
     MainAppRoutingModule,
     NgbModule,
     MatCardModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatFormFieldModule
   ]
 })
 export class MainAppModule { }
--- src/app/modules/main-app/pages/simulation/simulation.component.ts
+++ src/app/modules/main-app/pages/simulation/simulation.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-simulation',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimulationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goToHomePage() {
+    this.router.navigate(['planificacionPorcinos/home']);
   }
 
 }
